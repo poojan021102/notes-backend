4ed59a50eef3c1593a7dfa6a80d305901ee9945f
Poojan Patel <poojanpatel02112002@gmail.com>
2022-12-27 09:40:30 +0530

    Changed settings.py

--- notes/settings.py
+++ notes/settings.py
@@ -134,12 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
 'DEFAULT_AUTHENTICATION_CLASSES': (
